14d131a2e0aede62bbea31d982cd1ccfe258c932
nobobo1234 <noahvanboven@gmail.com>
2020-02-16 12:47:09 +0100

    Part 1 done

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,10 +1,18 @@
 extends Actor
 
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body: Node) -> void:
+	queue_free()
+
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -26,3 +34,14 @@ func calculate_move_velocity(
 		if is_jump_interrupted:
 			out.y = 0.0
 		return out
+
+func calculate_stomp_velocity(
+	linear_velocity: Vector2,
+	impulse: float
+) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
+
+
